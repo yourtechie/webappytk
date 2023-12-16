8027eb6ccc9f9a0dad8c58d17a2e77e8901b1b7e
Brookeycutie <brookeyajayi55@gmail.com>
2023-12-16 23:00:04 +0100

    Adding Paragraph

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     </head>
     <body>
     <h1>My Web App Deployment</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quisquam assumenda consequuntur illum soluta maiores minus repellendus itaque. Beatae repellendus quibusdam vero aliquid illo quam ullam minima blanditiis iste expedita.</p>
     </body>
 
 </html>
